f66aac81604b5ae28d72086784cc62381454c4f5
Antonio Ferreira <af.myhouse@gmail.com>
2023-11-24 01:02:06 +0000

    HP 20231125 001

--- s-pipes/pipes4-n.c
+++ s-pipes/pipes4-n.c
@@ -6,7 +6,7 @@
 /*   By: antoda-s <antoda-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 19:41:03 by antoda-s          #+#    #+#             */
-/*   Updated: 2023/11/24 00:35:00 by antoda-s         ###   ########.fr       */
+/*   Updated: 2023/11/24 01:00:24 by antoda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,6 @@ int	main(int argc, char **argv)
 	close(pipes[0][1]);
 
 
-
 	while (++i < PROCESS_NUM)
 	{
 		pids[i] = fork();
@@ -59,7 +58,8 @@ int	main(int argc, char **argv)
 			perror("fork");
 			exit(EXIT_FAILURE);
 		}
-		if (pids[i] == 0){
+		if (pids[i] == 0)
+		{
 			printf("\n[%d] child process created\n", i);
 			fflush(stdout);
 			j = -1;
@@ -67,13 +67,13 @@ int	main(int argc, char **argv)
 			{
 				if (j != i)
 				{
-					printf("i = [%d] A closing pipe[%d][0]\n", i, j);
+					printf("i = [%d] A closing pipe[%d][0] (%d)\n", i, j, pipes[j][0]);
 					fflush(stdout);
 					close(pipes[j][0]);
 				}
 				if (j != i + 1)
 				{
-					printf("i = [%d] B closing pipe[%d][1]\n", i, j);
+					printf("i = [%d] B closing pipe[%d][1] (%d)\n", i, j, pipes[j][1]);
 					fflush(stdout);
 					close(pipes[j][1]);
 				}
@@ -83,7 +83,7 @@ int	main(int argc, char **argv)
 				perror("read");
 				exit(EXIT_FAILURE);
 			}
-			printf("i = [%d] read pipe[%d][0] = %d\n", i, i, x);
+			printf("i = [%d] read  pipe[%d][0] = %d\n", i, i, x);
 			fflush(stdout);
 			x++;
 			if (write(pipes[i + 1][1], &x, sizeof(int)) == -1)
@@ -96,32 +96,29 @@ int	main(int argc, char **argv)
 			close(pipes[i][0]);
 			close(pipes[i + 1][1]);
 			return (0);
-			//break ;
 		}
 	}
 
-	// int y =5;
-	// printf("i = [P] write pipe[0][1] = %d\n", y);
-	// fflush(stdout);
-	// if (write(pipes[0][1], &y, sizeof(int)) == -1)
-	// {
-	// 	perror("write");
-	// 	exit(EXIT_FAILURE);
-	// }
-	// close(pipes[0][1]);
-
-	for (i = 0; i < PROCESS_NUM + 1; i++){
+	i = -1;
+	while (++i < PROCESS_NUM + 1)
+	{
 		wait(NULL);
 	}
 
-	if(read(pipes[PROCESS_NUM][0], &y, sizeof(int)) == -1)
+	if (read(pipes[PROCESS_NUM][0], &y, sizeof(int)) == -1)
 	{
 		perror("read");
 		exit(EXIT_FAILURE);
 	}
-	printf("\ni = [P] read pipe[%d][0] = %d\n", PROCESS_NUM, y);
+	printf("\ni = [P] read  pipe[%d][0] = %d\n", PROCESS_NUM, y);
 	fflush(stdout);
-	close(pipes[PROCESS_NUM][0]);
+	i = -1;
+	while (++i < PROCESS_NUM + 1)
+	{
+		close(pipes[i][1]);
+		close(pipes[i][0]);
+	}
 
+	//close(pipes[PROCESS_NUM][0]);
 	return (0);
 }
